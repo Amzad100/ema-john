e2aaed5d2e3981f18966b1a4d24e3107f70fc184
Amzad100 <mdamzad2003a@gmail.com>
2023-03-29 00:51:31 +0600

    cart calculator added

---
+++ src/Components/Cart/Cart.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Cart.css';
+
+const Cart = ({cart}) => {
+
+    let total = 0;
+    let totalShipping = 0;
+    for (const product of cart){
+        total = total + product.price;
+        totalShipping = totalShipping + product.shipping;
+    }
+
+    const tax = total *7 /100;
+
+    const GrantTotal = total + totalShipping + tax;
+
+    return (
+        <div className='cart'>
+            <h1>Cart summary</h1>
+            <p>Selected Item : {cart.length}</p>
+            <p>Total Price: ${total}</p>
+            <p>Total Shipping Charge: {totalShipping}</p>
+            <p>Tax: {tax.toFixed(2)}</p>
+            <p>Grand Total: {GrantTotal.toFixed(2)}</p>
+        </div>
+    );
+};
+
+export default Cart;
