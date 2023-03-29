41b143ff96c831224d0fed250fd9a29a24dce60a
Amzad100 <mdamzad2003a@gmail.com>
2023-03-29 16:11:23 +0600

    show data from local storage

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -5,9 +5,14 @@ const Cart = ({cart}) => {
 
     let total = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for (const product of cart){
-        total = total + product.price;
+        if (product.quantity == 0){
+            product.quantity = 1;
+        }
+        total = total + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
     }
 
     const tax = total *7 /100;
@@ -17,7 +22,7 @@ const Cart = ({cart}) => {
     return (
         <div className='cart'>
             <h1>Cart summary</h1>
-            <p>Selected Item : {cart.length}</p>
+            <p>Selected Item : {quantity}</p>
             <p>Total Price: ${total}</p>
             <p>Total Shipping Charge: ${totalShipping}</p>
             <p>Tax: ${tax.toFixed(2)}</p>
